0a69fd3f9a82a8fdd0e8dd19cacaa33b88e45f9c
Teresa E. Gimeno <tegimeno@gmail.com>
2019-12-11 18:17:52 +0100

    So many things

--- barkRead/climaALL.R
+++ barkRead/climaALL.R
@@ -1,7 +1,10 @@
 lengthNA <- function(x){
   return(length(x) - length(which(is.na(x))))
 }
-stationName <- 'Orduna'
+s.err <- function(x){
+  return(sd(x, na.rm = T)/sqrt(lengthNA(x)))
+}
+stationName <- 'Amurrio'
 Precip <- read.csv(paste0('barkData/meteo_Orduna/Pday_', stationName, '.csv'))
 Tmean <- read.csv(paste0('barkData/meteo_Orduna/TmeanDay_', stationName, '.csv'))
 Tmin <- read.csv(paste0('barkData/meteo_Orduna/TminDay_', stationName, '.csv'))
@@ -17,7 +20,39 @@ clim$Month_Y <- paste0(clim$Year, '-', clim$Month)
 climMonth <- dplyr::summarise(dplyr::group_by(clim, Month_Y),
                               Pmonth=sum(Pday_mm, na.rm = T), Pndays=lengthNA(Pday_mm),
                               TmeanMonth=mean(TmeanDay_C, na.rm = T), Tndays=lengthNA(TmeanDay_C),
-                              TminMonth=mean(TminDay_C, na.rm = T), TmaxMonth=mean(TmaxDay_C, na.rm = T),)
-climMonth <- as.data.frame(climMonth)
+                              TminMonth=mean(TminDay_C, na.rm = T), TmaxMonth=mean(TmaxDay_C, na.rm = T))
 climMonth$Pmonth <- ifelse(climMonth$Pndays <= 24, NA, climMonth$Pmonth)
 climMonth[which(climMonth$Tndays <= 24), c('TmeanMonth', 'TminMonth', 'TmaxMonth')] <- NA
+climMonth$Month <- substr(climMonth$Month_Y, 6, 8)
+climMonth$Year <- substr(climMonth$Month_Y, 1, 4)
+climMonthSumm <- dplyr::summarise(dplyr::group_by(climMonth, Month),
+                                  Pavg_month = mean(Pmonth, na.rm = T), Pse_month = s.err(Pmonth),
+                                  Pnmonths = lengthNA(Pmonth),
+                                  Tmean_avg_month = mean(TmeanMonth, na.rm = T),
+                                  Tmean_se_month = s.err(TmeanMonth),
+                                  Tmin_avg_month = mean(TminMonth, na.rm = T),
+                                  Tmin_se_month = s.err(TminMonth),
+                                  Tmax_avg_month = mean(TmaxMonth, na.rm = T),
+                                  Tmax_se_month = s.err(TmaxMonth),
+                                  Tnmonths = lengthNA(TmeanMonth))
+climYear <- dplyr::summarise(dplyr::group_by(clim, Year),
+                                          Pyear=sum(Pday_mm, na.rm = T), Pndays=lengthNA(Pday_mm),
+                                          TmeanYear=mean(TmeanDay_C, na.rm = T),
+                                          TminYear=mean(TminDay_C, na.rm = T),
+                                          TmaxYear=mean(TmaxDay_C, na.rm = T),
+                                          Tndays=lengthNA(TmeanDay_C))
+climYear[which(climYear$Pndays <= 293), 'Pyear'] <- NA 
+climYear[which(climYear$Tndays <= 293), c('TmeanYear', 'TminYear', 'TmaxYear')] <- NA
+climYearSumm <- dplyr::summarise(dplyr::group_by(climYear),
+                                  Pavg_year = mean(Pyear, na.rm = T), Pse_year = s.err(Pyear),
+                                  Pnyears = lengthNA(Pyear),
+                                  Tmean_avg_year = mean(TmeanYear, na.rm = T),
+                                  Tmean_se_year = s.err(TmeanYear),
+                                  Tmin_avg_year = mean(TminYear, na.rm = T),
+                                  Tmin_se_year = s.err(TminYear),
+                                  Tmax_avg_year = mean(TmaxYear, na.rm = T),
+                                  Tmax_se_year = s.err(TmaxYear),
+                                  Tnmonths = lengthNA(TmeanYear))
+
+climYearSumm
+climMonthSumm
--- barkRead/meteoOrdu.R
+++ barkRead/meteoOrdu.R
@@ -1,25 +1,26 @@
 fileNames <- as.list(paste0('barkData/meteo_orduna/detailed/', list.files('barkData/meteo_orduna/detailed/')))
 allRaw <- dplyr::bind_rows(lapply(fileNames, data.table::fread))
 names(allRaw) <- c(as.character(read.csv('barkData/meteo_Orduna/descripcion_datos.csv')[,2]))
-allRaw$Value1 <- strsplit(allRaw$MEDICION, ',')[[1]][1]
-allRaw$Value2 <- strsplit(allRaw$MEDICION, ',')[[1]][2]
+valuesL <- strsplit(allRaw$MEDICION, ',')
+allRaw$Value1 <- c(unlist(lapply(valuesL, function(L) L[1])))
+allRaw$Value2 <- c(unlist(lapply(valuesL, function(L) L[2])))
 write.csv(allRaw, file = 'barkData/meteo_Orduna/temp.csv', row.names = F)
 clean <- read.csv('barkData/meteo_Orduna/temp.csv')
+clean[which(is.na(clean$Value2)), 'Value2'] <- 0
+clean$Value2 <- ifelse(clean$Value2 <= 9, clean$Value2 * 10, clean$Value2)
 clean$value <- clean$Value1 + clean$Value2*0.01
 clean$DateTime <- paste0(clean$FECHA_MEDICION, ' ', clean$HORA_MEDICION)
 clean$DateTime <- lubridate::dmy_hm(as.character(clean$DateTime))
 clean <- doBy::orderBy(~DateTime, clean)
 codes <- read.csv('barkData/meteo_Orduna/codigo_meteoro.csv')[, c('CODIGO_METEORO', 'short')]
-clean <- merge(clean, codes, by = 'CODIGO_METEORO', all.x = T, all.y = F)[,c('DateTime', 'short', 'value')]
+clean <- dplyr::left_join(clean, codes, by = 'CODIGO_METEORO')[,c('DateTime', 'short', 'value')]
 library(dplyr)
 library(tidyr)
 meteo <- clean
 meteo <- clean %>% spread(short, value)
-
-stocks <- data.frame(
-  time = as.Date('2009-01-01') + 0:9,
-  X = rnorm(10, 0, 1),
-  Y = rnorm(10, 0, 2),
-  Z = rnorm(10, 0, 4)
-)
-stocksm <- stocks %>% gather(stock, price, -time)
+meteo$Date <- as.Date(meteo$DateTime)
+meteo$VPD <- 0.61365 * exp(17.502 * meteo$AirT_avg/(240.97 + meteo$AirT_avg)) * (1 -(meteo$RH_avg/100))
+meteoDay <- summarise(group_by(meteo, Date), Pday = sum(P_tot),
+                      Tmean = mean(AirT_avg), Tmin = min(AirT_avg),
+                      Tmax = max(AirT_avg),
+                      vpd = mean(VPD), vpdMax = max(VPD))
